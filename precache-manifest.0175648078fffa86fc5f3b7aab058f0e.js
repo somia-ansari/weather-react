9decc8e400b2de330de6b40f1563baa34c108c9a
Somia Sohail Ansari <ansarisomia@gmail.com>
2019-09-25 23:24:03 +0500

    Updates

---
+++ precache-manifest.0175648078fffa86fc5f3b7aab058f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a81de530a64e3f6ee64a31f1d2ff3a",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "0602e77f062d7d50007d",
+    "url": "/weather-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5a8b838a2fba350b21f3",
+    "url": "/weather-react/static/js/2.3a22ea94.chunk.js"
+  },
+  {
+    "revision": "0602e77f062d7d50007d",
+    "url": "/weather-react/static/js/main.528ddb48.chunk.js"
+  },
+  {
+    "revision": "4ee82424f29242dbef7d",
+    "url": "/weather-react/static/js/runtime-main.7674592e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/weather-react/static/media/logo.25bf045c.svg"
+  }
+]);
